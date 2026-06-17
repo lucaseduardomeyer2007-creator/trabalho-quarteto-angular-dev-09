99c1a8274d8242d0d53ee4b4c41880eb572cd892
lucaseduardomeyer2007-creator <lucaseduardomeyer2007@gmail.com>
2026-06-17 20:21:41 -0300

    Cadastrar feito com sucesso

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { BrinquedoCadastrar } from './brinquedos/brinquedo-cadastrar/brinquedo-cadastrar';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "brinquedos/cadastrar", loadComponent: () => BrinquedoCadastrar},
+];
--- src/app/brinquedos/brinquedo-cadastrar/brinquedo-cadastrar.ts
+++ src/app/brinquedos/brinquedo-cadastrar/brinquedo-cadastrar.ts
@@ -1,9 +1,50 @@
-import { Component } from '@angular/core';
-
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrinquedoModel } from '../../models/brinquedo.model';
 @Component({
   selector: 'app-brinquedo-cadastrar',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './brinquedo-cadastrar.html',
   styleUrl: './brinquedo-cadastrar.scss',
 })
-export class BrinquedoCadastrar {}
+export class BrinquedoCadastrar {
+  brinquedo = signal<BrinquedoModel>({
+    id: crypto.randomUUID(),
+    modelo: "",
+    marca: "",
+    preco: "",
+    quantidade: ""
+  })
+
+  salvar(): void {
+    const brinquedos = this.carregarBrinquedosDoLocalStorage();
+
+    brinquedos.push(this.brinquedo());
+
+    const brinquedosSring = JSON.stringify(brinquedos);
+    localStorage.setItem("brinquedos", brinquedosSring);
+
+    alert("Brinquedo cadastrado com sucesso");
+    this.limparCampos();
+  }
+
+  limparCampos(): void {
+    this.brinquedo.set({
+    id: "",
+    modelo: "",
+    marca: "",
+    preco: "",
+    quantidade: ""
+    })
+  }
+
+  carregarBrinquedosDoLocalStorage(): BrinquedoModel[]{
+    const brinquedosSring = localStorage.getItem("brinquedos");
+
+    if (brinquedosSring === null){
+      return [];
+    }
+
+    return JSON.parse(brinquedosSring) as BrinquedoModel[];
+  }
+}
