81b10e695a4dd38ba7f6b8bae4103213b35c277a
lucaseduardomeyer2007-creator <lucaseduardomeyer2007@gmail.com>
2026-06-17 21:41:42 -0300

    curso

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { BrinquedoCadastrar } from './brinquedos/brinquedo-cadastrar/brinquedo-cadastrar';
 import { BrinquedoListar } from './brinquedos/brinquedo-listar/brinquedo-listar';
+import { BrinquedoEditar } from './brinquedos/brinquedo-editar/brinquedo-editar';
 
 export const routes: Routes = [
     { path: "brinquedos/cadastrar", loadComponent: () => BrinquedoCadastrar},
-    { path: "brinquedos", loadComponent: () => BrinquedoListar}
+    { path: "brinquedos", loadComponent: () => BrinquedoListar},
+    { path: "brinquedos/editar/:id", loadComponent: () => BrinquedoEditar}
 ];
--- src/app/brinquedos/brinquedo-editar/brinquedo-editar.ts
+++ src/app/brinquedos/brinquedo-editar/brinquedo-editar.ts
@@ -1,9 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrinquedoModel } from '../../models/brinquedo.model';
+import { ActivatedRoute, Router, } from '@angular/router';
 
 @Component({
   selector: 'app-brinquedo-editar',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './brinquedo-editar.html',
   styleUrl: './brinquedo-editar.scss',
 })
-export class BrinquedoEditar {}
+export class BrinquedoEditar {
+  brinquedo = signal<BrinquedoModel>({
+    id: "",
+    modelo: "",
+    marca: "",
+    preco: "",
+    quantidade: ""
+  })
+
+
+  constructor(private activeRoute: ActivatedRoute, private router: Router) {
+
+    const idParaEditar = activeRoute.snapshot.paramMap.get("id");
+
+    const brinquedosString = localStorage.getItem("brinquedos")
+
+    if (brinquedosString === null) {
+      alert("Nenhum Brinquedo Cadastrado")
+      router.navigate(["/brinquedos"])
+      return
+    }
+
+    const brinquedos = JSON.parse(brinquedosString) as BrinquedoModel[];
+
+    const brinquedosEncontrados = brinquedos.filter(brinquedo => brinquedo.id === idParaEditar);
+
+    if (brinquedosEncontrados.length === 0) {
+      alert("Brinquedo não encontrado");
+      router.navigate(["/brinquedos"])
+      return;
+    }
+
+    this.brinquedo.set(brinquedosEncontrados[0]);
+
+  }
+
+  salvar(): void {
+
+    const brinquedos = this.carregarBrinquedosDoLocalStorage();
+
+    const indiceBrinquedosParaEditar = brinquedos.findIndex(x => x.id === this.brinquedo().id);
+
+    brinquedos[indiceBrinquedosParaEditar].modelo = this.brinquedo().modelo;
+    brinquedos[indiceBrinquedosParaEditar].marca = this.brinquedo().marca;
+    brinquedos[indiceBrinquedosParaEditar].preco = this.brinquedo().preco;
+    brinquedos[indiceBrinquedosParaEditar].quantidade = this.brinquedo().quantidade;
+
+
+    const brinquedosString = JSON.stringify(brinquedos)
+    localStorage.setItem("brinquedos", brinquedosString)
+
+    alert("Produto alterado com sucesso");
+
+    this.router.navigate(["/brinquedos"]);
+
+  }
+
+  carregarBrinquedosDoLocalStorage(): BrinquedoModel[] {
+
+    const brinquedosString =  localStorage.getItem("brinquedos");
+
+    if(brinquedosString === null) {
+
+      return[];
+    }
+    return JSON.parse(brinquedosString) as BrinquedoModel[];
+
+  }
+
+
+
+
+}
