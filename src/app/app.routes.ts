671b01794150b5512c429dc891568081c6f6bc85
erick janzen <erickjanzen554@gmail.com>
2026-06-17 21:21:28 -0300

    Adicionado  e Implementado o component brinquedo-listar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { BrinquedoCadastrar } from './brinquedos/brinquedo-cadastrar/brinquedo-cadastrar';
+import { BrinquedoListar } from './brinquedos/brinquedo-listar/brinquedo-listar';
 
 export const routes: Routes = [
     { path: "brinquedos/cadastrar", loadComponent: () => BrinquedoCadastrar},
+    { path: "brinquedos", loadComponent: () => BrinquedoListar}
 ];
--- src/app/brinquedos/brinquedo-listar/brinquedo-listar.ts
+++ src/app/brinquedos/brinquedo-listar/brinquedo-listar.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrinquedoModel } from '../../models/brinquedo.model';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-brinquedo-listar',
-  imports: [],
+  imports: [FormsModule, RouterLink],
   templateUrl: './brinquedo-listar.html',
   styleUrl: './brinquedo-listar.scss',
 })
-export class BrinquedoListar {}
+export class BrinquedoListar {
+  brinquedos = signal<BrinquedoModel[]>([]);
+
+  ngOnInit() {
+    this.carregarBrinquedosDoLocalStorage();
+  }
+
+  carregarBrinquedosDoLocalStorage(): void {
+    const brinquedosString = localStorage.getItem("brinquedos");
+    if (brinquedosString === null){
+      return;
+    }
+
+    const brinquedosLista = JSON.parse(brinquedosString) as BrinquedoModel[];
+
+    const brinquedosOrdenados = brinquedosLista.sort((x, y) => x.modelo.localeCompare(y.modelo));
+    this.brinquedos.set(brinquedosOrdenados);
+  }
+
+  apagar(id : string): void {
+    this.brinquedos.update(brinquedos => brinquedos.filter(x => x.id !== id))
+    const brinquedosString = JSON.stringify(this.brinquedos());
+    localStorage.setItem("brinquedos", brinquedosString);
+  }
+
+}
